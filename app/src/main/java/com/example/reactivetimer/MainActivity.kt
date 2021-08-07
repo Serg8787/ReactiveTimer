7fb8922639a6ec0a016ba3ae940fbe249da598b6
Serg8787 <tsybulnikserg@gmail.com>
2021-08-07 12:13:39 +0300

    add picker

---
+++ app/src/main/java/com/example/reactivetimer/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.reactivetimer
+
+import android.os.Bundle
+import android.os.SystemClock
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.NumberPicker
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.internal.operators.observable.ObservableCreate
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var minutes: Int = 0
+    var seconds: Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        numberPickerMinutes.minValue = 0
+        numberPickerMinutes.maxValue = 60
+        numberPickerMinutes.setOnValueChangedListener(object : NumberPicker.OnValueChangeListener{
+            override fun onValueChange(picker: NumberPicker?, oldVal: Int, newVal: Int) {
+               tvMin.text = String.format(newVal.toString())
+                Log.d("MyLOG",picker.toString())
+                minutes = picker!!.value
+            }
+        })
+
+
+        btStart.setOnClickListener {
+
+            val dispose = getSeconds().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe() {
+                    val min = it / 60
+                    tvMin.text = min.toString()
+                    tvSec.text = (it % 60).toString()
+                }
+        }
+    }
+
+    fun getSeconds(): Observable<Int> {
+        return ObservableCreate { subcrumber ->
+            for (i in minutes downTo 0) {
+                SystemClock.sleep(1000)
+                subcrumber.onNext(i)
+            }
+        }
+
+    }
+
+
+}
